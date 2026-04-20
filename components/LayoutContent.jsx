9a2bdee4c2e78052db9b8ad45b8bb58c05468017
Valentin Van Hove <valentinvanhove@Macbook-Air-de-Valentin.local>
2026-04-20 09:31:11 +0200

    Fix hydration error: disable Footer lazy loading

--- components/LayoutContent.jsx
+++ components/LayoutContent.jsx
@@ -1,18 +1,14 @@
 'use client'
 
-import { lazy, Suspense } from 'react'
 import Navbar from './Navbar'
-
-const Footer = lazy(() => import('./Footer'))
+import Footer from './Footer'
 
 export default function LayoutContent({ children }) {
   return (
     <>
       <Navbar />
       <main className="min-h-screen">{children}</main>
-      <Suspense fallback={<div className="h-20 bg-gray-900" />}>
-        <Footer />
-      </Suspense>
+      <Footer />
     </>
   )
 }
